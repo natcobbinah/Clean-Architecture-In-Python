e1197feaa52d45cd387c111f8ffa496f32471d9e
Nathaniel Cobbinah <nathaniel.cobbinah2@ten.com>
2025-04-03 16:15:49 +0200

    entities, use-cases, tests, wsgi integration, external systems interfacing completed

---
+++ rentomatic/tests/domain/test_room.py
@@ -0,0 +1,18 @@
+import uuid 
+from rentomatic.domain.room import Room 
+
+def test_room_model_init():
+    code = uuid.uuid4() 
+
+    init_dict = {
+        "code": code, 
+        "size": 200,
+        "price": 10,
+        "longitude": -0.09998975,
+        "latitude": 51.75436293,
+    }
+
+    room1 = Room.from_dict(init_dict)
+    room2 = Room.from_dict(init_dict)
+
+    assert room1 == room2
